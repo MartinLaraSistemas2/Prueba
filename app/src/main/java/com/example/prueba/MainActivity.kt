5eb1d3173cc36c10ec161d3649ef5a849c7f5b70
MartinLaraSistemas2 <126721335+MartinLaraSistemas2@users.noreply.github.com>
2023-03-18 20:17:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.prueba
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.prueba.ui.theme.PruebaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PruebaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+
+    Column(){
+    Text(text = "Hello $name!")
+    Text(text = "Hello")
+    Text(text = "ya vamonos")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PruebaTheme {
+        Greeting("Android")
+    }
+}
